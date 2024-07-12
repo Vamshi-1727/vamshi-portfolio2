6dd03ddf74ec1895aff70e1d87ee7baa2b829ace
Vamshi-1727 <vv461284@gmail.com>
2024-07-12 12:18:48 +0530

    vamshiportfolio2

---
+++ About.js
@@ -0,0 +1,15 @@
+// src/pages/About.js
+import React from 'react';
+
+function About() {
+  return (
+    <div className="about">
+      <h1>About Me</h1>
+      <p>Name:vamshi</p>
+      <p>Email:vv461284@gmail.com</p>
+      <p>Mobile:9959561810</p>
+    </div>
+  );
+}
+
+export default About;
---
+++ Contact.js
@@ -0,0 +1,15 @@
+// src/pages/Contact.js
+import React from 'react';
+
+function Contact() {
+  return (
+    <div className="contact">
+      <h1>Contact</h1>
+      <p>Please feel free to reach out to me via email or phone.</p>
+      <p>Email:vv461284@gmail.com</p>
+      <p>Phone:9959561810</p>
+    </div>
+  );
+}
+
+export default Contact;
